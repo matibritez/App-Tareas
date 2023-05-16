d009fe4dafcffd130a8026693fa2adb934aaf9b7
Matias Britez <89950148+matibritez@users.noreply.github.com>
2023-05-15 22:35:56 -0300

    tareas

--- app.js
+++ app.js
@@ -19,6 +19,31 @@ function saveTask(e){
     tasks.push(task);
     localStorage.setItem('tasks', JSON.stringify(tasks));
   }
+
+  getTasks();
   e.preventDefault();
 } 
 
+function getTasks(){
+  let tasks = JSON.parse(localStorage.getItem('tasks'));
+  let tasksView = document.getElementById('tasks');
+
+  tasksView.innerHTML = ``;
+
+  for(let i = 0 ; i < tasks.length; i++){
+    let title = tasks[i].title;
+    let description = tasks[i].description;
+
+    tasksView.innerHTML += `<div class="card mb-4">
+      <div class="card-body">
+        <p>${title} - ${description}</p>
+        <a class="btn btn-danger">
+          Delete
+        <a/>
+      </div>
+    </div>`
+  }
+}
+
+
+getTasks();
